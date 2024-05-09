9cc7f80ebf8c093ffc1d88c789628f683cc2692d
Wagner Lopes <wagnerlopesbr@hotmail.com>
2024-05-09 15:02:12 -0300

    Req 6/7 v1

--- inventory_report/reports/report.py
+++ inventory_report/reports/report.py
@@ -1,2 +1,10 @@
-class Report:
-    pass
+from inventory_report.inventory import Inventory
+from typing import Protocol
+
+
+class Report(Protocol):
+    def add_inventory(self, inventory: Inventory) -> None:
+        ...
+
+    def generate(self) -> str:
+        ...
