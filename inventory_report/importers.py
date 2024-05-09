2b7446a303c817e0237fe13c418c3c93c25d79fa
Wagner Lopes <wagnerlopesbr@hotmail.com>
2024-05-09 14:23:21 -0300

    BONUS Req 8 v2 Refactoring

--- inventory_report/importers.py
+++ inventory_report/importers.py
@@ -21,22 +21,24 @@ class JsonImporter(Importer):
 
 
 class CsvImporter(Importer):
+    # commented code does the same as the return line inside the "with" block
     def import_data(self) -> List[Product]:
-        products = []
+        # products = []
         with open(self.path, "r", newline="") as f:
-            reader = csv.DictReader(f)
-            for row in reader:
-                product = Product(
-                    id=row["id"],
-                    product_name=row["product_name"],
-                    company_name=row["company_name"],
-                    manufacturing_date=row["manufacturing_date"],
-                    expiration_date=row["expiration_date"],
-                    serial_number=row["serial_number"],
-                    storage_instructions=row["storage_instructions"]
-                )
-                products.append(product)
-        return products
+            return [Product(**product) for product in csv.DictReader(f)]
+        #     reader = csv.DictReader(f)
+        #     for row in reader:
+        #         product = Product(
+        #             id=row["id"],
+        #             product_name=row["product_name"],
+        #             company_name=row["company_name"],
+        #             manufacturing_date=row["manufacturing_date"],
+        #             expiration_date=row["expiration_date"],
+        #             serial_number=row["serial_number"],
+        #             storage_instructions=row["storage_instructions"]
+        #         )
+        #         products.append(product)
+        # return products
 
 
 # Não altere a variável abaixo
