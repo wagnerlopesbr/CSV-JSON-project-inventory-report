d430198e883ab286ecf3a1363d8133f422659d9c
Wagner Lopes <wagnerlopesbr@hotmail.com>
2024-05-09 13:41:40 -0300

    Req 2/7 v1

--- tests/product_report/test_product_report.py
+++ tests/product_report/test_product_report.py
@@ -1,2 +1,21 @@
+from inventory_report.product import Product
+
+
 def test_product_report() -> None:
-    raise NotImplementedError
+    test_product = Product(
+        id="1",
+        product_name="Gabigol de Pelúcia",
+        company_name="Flamengo",
+        manufacturing_date="2024-05-09",
+        expiration_date="2034-05-09",
+        serial_number="123456",
+        storage_instructions="Cuidar com carinho",
+    )
+
+    assert (
+        str(test_product)
+        == "The product 1 - Gabigol de Pelúcia with serial number 123456 "+
+        "manufactured on 2024-05-09 by the company Flamengo "+
+        "valid until 2034-05-09 must be stored according to "+
+        "the following instructions: Cuidar com carinho."
+    )
