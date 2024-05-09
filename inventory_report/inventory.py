5fae9d1fa418cedbb9158e7d6eec4683a448b4fd
Wagner Lopes <wagnerlopesbr@hotmail.com>
2024-05-09 14:48:01 -0300

    Req 5/7 v1

--- inventory_report/inventory.py
+++ inventory_report/inventory.py
@@ -1,2 +1,17 @@
+from typing import List, Optional
+from inventory_report.product import Product
+
+
 class Inventory:
-    pass
+    @property
+    def data(self) -> List[Product]:
+        return self.__data
+
+    def __init__(self, data: Optional[List[Product]] = None) -> None:
+        self.__data = data or []
+
+    def add_data(self, product: Product) -> None:
+        self.__data.append(product)
+        # append() would add a list of products as a single product
+        # extend() would add a list of products but the Product class
+        #   would have to be iterable (which it isn't) and I can't change it
