da3f020e9b287097e45246695f979d4bc1751bdf
Wagner Lopes <wagnerlopesbr@hotmail.com>
2024-05-09 16:10:41 -0300

    BONUS Req 9 v1

--- inventory_report/reports/complete_report.py
+++ inventory_report/reports/complete_report.py
@@ -1,2 +1,30 @@
-class CompleteReport:
-    pass
+from .simple_report import SimpleReport
+from collections import Counter
+
+
+class CompleteReport(SimpleReport):
+    def find_stocked_products_by_company(self) -> Counter[str]:
+        company_names = [
+            product.company_name
+            for inventory in self.inventories
+            for product in inventory.data
+        ]
+        return Counter(company_names)
+
+    def generate(self) -> str:
+        oldest_manufacturing_date = self.find_oldest_manufacturing_date()
+        closest_expiration_date = self.find_closest_expiration_date()
+        largest_inventory_co = self.find_largest_inventory_company()
+        stocked_products_by_company = self.find_stocked_products_by_company()
+
+        output = (
+            f"Oldest manufacturing date: {oldest_manufacturing_date}\n"
+            f"Closest expiration date: {closest_expiration_date}\n"
+            f"Company with the largest inventory: {largest_inventory_co}\n"
+            f"Stocked products by company:\n"
+        )
+
+        for company, count in stocked_products_by_company.items():
+            output += f"- {company}: {count}\n"
+
+        return output
